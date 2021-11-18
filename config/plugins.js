9aaffc5f1b5b0be8b7d3b12547df088db9287bdf
Pedro Coelho <pedrohenriquesc@gmail.com>
2021-11-18 15:45:52 -0300

    Initial Commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => ({
-  // ...
   upload: {
     provider: "cloudinary",
     providerOptions: {
@@ -8,5 +7,4 @@ module.exports = ({ env }) => ({
       api_secret: env("CLOUDINARY_SECRET"),
     },
   },
-  // ...
 });
